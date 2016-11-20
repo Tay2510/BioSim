4d921dd1c85f24ec4c717a594f3547a378e67386
amenaged1 <amenaged1@users.noreply.github.com>
2016-11-20 11:37:17 -0500

    Add files via upload

---
+++ p53Simulate.m
@@ -0,0 +1,22 @@
+function [ output_args ] = p53Simulate( delta_t, T, initial )
+%p53Simulate Runs simulation and outputs graph of p53 concentrations
+%   Input initial concentrations?
+%   initial - vector of initial concentrations.
+
+results = zeros(T/delta_t+1, length(initial));
+results(1, :) = initial;
+
+time_vector = 0:delta_t:T;
+
+for i = 2:T/delta_t+1
+    
+    
+    
+end
+
+%edit to only include p53 and modified p53 concentrations?
+plot(time_vector, results);
+title('p53 and Modified p53 Concentrations');
+
+end
+
