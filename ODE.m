6c28dd56e645d7738958a20248a38b7cddd94ae8
liangxt <liangxt07@gmail.com>
2016-12-05 19:37:22 -0500

    Change ARF_MDM2 value

--- ODE.m
+++ ODE.m
@@ -1,8 +1,7 @@
 clc
 clear all;
-close all;
 
-T_end = 20;
+T_end = 10;
 delta_t = 0.01;
 N = T_end/delta_t;
 timeline = 0:delta_t:(T_end - delta_t);
@@ -15,9 +14,10 @@ E2F1_initial = 0.9;
 RBp_initial = 1.88;
 RB_E2F1_initial = 0;
 RB_initial = 0;
+
 p53helper_initial = 0.39;
 p53killer_initial = 0.39;
-DYRK2_initial = 2;
+DYRK2_initial = 1;
 
 DYRK2 = zeros(1, N);
 p53helper = zeros(1, N);
@@ -29,7 +29,6 @@ E2F1 = zeros(1, N);
 RBp = zeros(1, N);
 RB_E2F1 = zeros(1, N);
 RB = zeros(1, N);
-
 p53killer(1) = p53killer_initial;
 p53helper(1) = p53helper_initial;
 MDM2(1) = MDM2_initial;
@@ -41,30 +40,61 @@ RB_E2F1(1) = RB_E2F1_initial;
 RB(1) = RB_initial;
 DYRK2(1) = DYRK2_initial;
 
-kdpp53_initial = 0.5;
-kdsRE_initial = 1;
-
-kdpp53 = kdpp53_initial;
-kdsRE = kdsRE_initial;
-
-for i = 2 : N      
+for i = 2 : N        
     DYRK2(i) = DYRK2(i - 1) + delta_t * (-0.0003 * DYRK2(i - 1));
-    p53helper(i) = p53helper(i - 1) + delta_t * (-DYRK2(i - 1) * p53helper(i - 1) / (0.1 + p53helper(i - 1)) + 0.5 - (0.1 + MDM2(i - 1)) * p53helper(i - 1) + kdpp53 * p53killer(i - 1) / (0.1 + p53killer(i - 1)));
-    p53killer(i) = p53killer(i - 1) + delta_t * (DYRK2(i - 1) * p53helper(i - 1) / (0.1 + p53helper(i - 1)) - kdpp53 * p53killer(i - 1) / (0.1 + p53killer(i - 1)) - (0.1 + MDM2(i - 1) * p53killer(i - 1)));
-    MDM2(i) = MDM2(i - 1) + delta_t * (0.02 + 0.3 * (p53killer(i - 1) + p53helper(i - 1)) - 0.1 * MDM2(i - 1) - 10 * ARF(i - 1) * MDM2(i - 1) + 2.1 * ARF_MDM2(i - 1));
-    ARF(i) = ARF(i - 1) + delta_t * (0.01 + 0.3 * E2F1(i - 1) - 0.1 * ARF(i - 1) - 10 * ARF(i - 1) * MDM2(i - 1) + 2.1 * ARF_MDM2(i - 1));
-    ARF_MDM2(i) = ARF_MDM2(i - 1) + delta_t * (10 * ARF(i - 1) * MDM2(i - 1) - 2 * ARF_MDM2(i - 1) - 0.1 * ARF_MDM2(i - 1) - 0.1 * ARF_MDM2(i - 1));
-    E2F1(i) = E2F1(i - 1) + delta_t * (-5 * RB(i - 1) * E2F1(i - 1) + kdsRE * RB_E2F1(i - 1));
-    RBp(i) = sigma + RBp(i - 1) + delta_t * (-0.5 * RBp(i - 1) / (0.1 + RBp(i - 1)));
-    RB_E2F1(i) = 1 - E2F1(i);
-    RB(i) = 2 - RBp(i) - RB_E2F1(i);  
-    if i == N / 2
-       ratio1 = p53helper(i) / p53killer(i); 
+    if DYRK2(i) < 0
+        DYRK2(i) = 0;
     end
+    p53helper(i) = p53helper(i - 1) + delta_t * (-DYRK2(i - 1) * ...
+        p53helper(i - 1) / (0.1 + p53helper(i - 1)) + 0.5 - ...
+        (0.1 + MDM2(i - 1)) * p53helper(i - 1) + 0.5 * p53killer(i - 1) ...
+        /(0.1 + p53killer(i - 1)));
+    if p53helper(i) < 0
+       p53helper(i) = 0;
+    end
+    p53killer(i) = p53killer(i - 1) + delta_t * (DYRK2(i - 1) * ...
+        p53helper(i - 1) / (0.1 + p53helper(i - 1)) - 0.5 * ...
+        p53killer(i - 1) / (0.1 + p53killer(i - 1)) - (0.1 + MDM2(i - 1) * ...
+        p53killer(i - 1)));
+    if p53killer(i) < 0
+        p53killer(i) = 0;
+    end
+    MDM2(i) = MDM2(i - 1) + delta_t * (0.02 + 0.3 * (p53killer(i - 1) ...
+        + p53helper(i - 1)) - 0.1 * MDM2(i - 1) - 10 * ARF(i - 1) * ...
+        MDM2(i - 1) + 2.1 * ARF_MDM2(i - 1));
+    if MDM2(i) < 0
+        MDM2(i) = 0;
+    end
+    ARF(i) = ARF(i - 1) + delta_t * (0.01 + 0.3 * E2F1(i - 1) - 0.1 ...
+        * ARF(i - 1) - 10 * ARF(i - 1) * MDM2(i - 1) + 2.1 ...
+        * ARF_MDM2(i - 1));
+    if ARF(i) < 0
+        ARF(i) = 0;
+    end
+    ARF_MDM2(i) = ARF_MDM2(i - 1) + delta_t * (10 * ARF(i - 1) * ...
+        MDM2(i - 1) - 2 * ARF_MDM2(i - 1) - 0.1 * ARF_MDM2(i - 1) - ...
+        0.1 * ARF_MDM2(i - 1));
+    if ARF_MDM2(i) < 0
+        ARF_MDM2(i) = 0;
+    end
+    E2F1(i) = E2F1(i - 1) + delta_t * (-5 * RB(i - 1) * E2F1(i - 1) + ...
+        RB_E2F1(i - 1));
+    if E2F1(i) < 0
+        E2F1(i) = 0;
+    end
+    RBp(i) = sigma + RBp(i - 1) + delta_t * (-0.5 * RBp(i - 1) / ...
+        (0.1 + RBp(i - 1)));
+    if RBp(i) < 0
+        RBp(i) = 0;
+    end
+    RB_E2F1(i) = 1 - E2F1(i);
+    RB(i) = 2 - RBp(i) - RB_E2F1(i);
 end
 
+
 %% Plot
-plot(timeline, p53killer, timeline, p53helper, timeline, MDM2, timeline, ARF, timeline, RB, timeline, RBp);
+plot(timeline, p53killer, timeline, p53helper, timeline, MDM2, ...
+    timeline, ARF, timeline, RB, timeline, RBp);
 legend('p53_k_i_l_l_e_r','p53_h_e_l_p_e_r','MDM2','ARF', 'RB', 'RBp');
 xlabel('time');
 ylabel('concentration');
